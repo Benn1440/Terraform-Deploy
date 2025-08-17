57d9a7f620c797e7827721d67465475412d62179
Benedict Okafor <benedict.okafor@fonyou.com>
2025-08-17 13:46:52 +0100

    Updated outputs file

--- main.tf
+++ main.tf
@@ -6,20 +6,29 @@ resource "random_string" "site_suffix" {
   special = false
 }
 
-resource "netlify_deploy_key" "key" {
-  # Optionally customize
-}
+# resource "netlify_deploy_key" "key" {
+#   # Optionally customize
+# }
 
-resource "netlify_site" "app_site" {
-  name = "${var.site_name}-${random_string.site_suffix.id}"
+# resource "netlify_site" "app_site" {
+#   name = "${var.site_name}-${random_string.site_suffix.id}"
 
-  repo {
-    provider       = "github"
-    repo_path      = "${var.github_username}/${var.github_repo}"
-    repo_branch    = "main"
-    build_dir      = "/app"
-    deploy_key_id  = netlify_deploy_key.key.id
-  }
-}
+#   repo {
+#     provider       = "github"
+#     repo_path      = "${var.github_username}/${var.github_repo}"
+#     repo_branch    = "main"
+#     build_dir      = "/app"
+#     deploy_key_id  = netlify_deploy_key.key.id
+#   }
+# }
 
+
+# resource "netlify_site_build_settings" "app_site" {
+
+#   site_id                = data.netlify_site.app_site.id
+#   build_command          = "npm run build"
+#   publish_directory      = "dist"
+#   production_branch      = "master"
+#   branch_deploy_branches = ["preview", "master"]
+# }
 
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 # This output provides the final URL of the live site.
 # It makes it easy for the user to find the deployed site.
-output "site_url" {
-  description = "The live URL of the deployed Netlify site."
-  value       = netlify_site.app_site.URL
-}
+# output "site_url" {
+#   description = "The live URL of the deployed Netlify site."
+#   value       = netlify_site.app_site.URL
+# }
